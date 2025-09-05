b9e41b0454d3c251af158830f6342dad58d5a6f3
mayra2014 <seemapahuja7@gmail.com>
2025-09-05 14:43:04 +0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,28 @@
+name="Mayra"
+Age=11
+Weight=36.1
+Student=True
+
+print("name is ", name)
+print("the data type of name is ", type(name) )
+
+print("Age is", Age)
+print(" the data type of age is ", type (Age))
+
+print("Weight is", Weight) 
+print( "the data type of age is ", type( Weight ) )
+
+print ("Student is", Student)
+print( "the data type of age is",type (Student) )
+
+
+number1=2937450928347509
+number2=1032823412341234
+
+print("addition", number1+number2 )
+print("subtraction", number1-number2)
+print("multiplication",number1*number2)
+print("division",number1/number2)
+print("floordivision", number1//number2)
+print("modules",number1%number2)
+print("exponention",number1**number2)
